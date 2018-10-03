914c7d001b58b34cbd3291f06614004d494a40c7
Atul Bhosale <atul1bhosale@gmail.com>
2018-10-03 11:52:05 +0530

    Rename kube-arbitrator to kube-batch
    Signed-off-by: Atul Bhosale <atul1bhosale@gmail.com>
    Signed-off-by: Da K. Ma <klaus1982.cn@gmail.com>

--- pkg/scheduler/cache/interface.go
+++ pkg/scheduler/cache/interface.go
@@ -19,7 +19,7 @@ package cache
 import (
 	"k8s.io/api/core/v1"
 
-	"github.com/kubernetes-incubator/kube-arbitrator/pkg/scheduler/api"
+	"github.com/kubernetes-sigs/kube-batch/pkg/scheduler/api"
 )
 
 // Cache collects pods/nodes/queues information
